281e4932825680dfcd2aa4732098609ea0df55df
OlgaGatalskaya <gatalolia86@gmail.com>
2020-06-08 20:09:43 +0200

    add docs

---
+++ docs/precache-manifest.86e904e2264f5244dbc0785e880655ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb69ffbb232461ca1bd0cdb401e7cffe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "812cbe694cb59cc4d381",
+    "url": "./static/css/main.5cafb12c.chunk.css"
+  },
+  {
+    "revision": "3fe9f8a1d4fade360c43",
+    "url": "./static/js/2.731454ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.731454ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812cbe694cb59cc4d381",
+    "url": "./static/js/main.7da64ab1.chunk.js"
+  },
+  {
+    "revision": "c27a392e8588e2888998",
+    "url": "./static/js/runtime-main.c19cbf81.js"
+  }
+]);
